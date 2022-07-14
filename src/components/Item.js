c2b25179800326af1d365459aaae4f8005ba4955
Oscar-Otee <oluoch.oscar.oo@gmail.com>
2022-07-14 13:45:18 +0300

    Assignment Complete

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+//import { useState } from "react/cjs/react.production.min";
 
 function Item({ name, category }) {
+  const [isInCart, setIsIncart] = useState(false);
+
+  function handleAddToCartClick() {
+    setIsIncart((isInCart) => !isInCart);
+  }
   return (
-    <li className="">
+    <li className={isInCart? "in-cart": ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={isInCart? "remove-from-cart": "add-to-cart"} 
+        onClick={handleAddToCartClick}>
+        {isInCart? "Remove From": "Add to"} Cart
+      </button>
     </li>
   );
 }
 
 export default Item;
+
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+//import { useState } from "react/cjs/react.production.min";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  function handleCategoryChange(e) {
+    setSelectedCategory(e.target.value);
+  }
+
+const itemsToDisplay = items.filter((item) => {
+
+    if (selectedCategory === "All") return true;
+
+    return item.category === selectedCategory;
+})
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleCategoryChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +26,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
@@ -22,3 +35,4 @@ function ShoppingList({ items }) {
 }
 
 export default ShoppingList;
+
